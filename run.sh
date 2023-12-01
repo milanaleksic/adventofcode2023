0ace60bfb6354a14dc0ebb9d57942faf1e65a839
Milan Aleksić <milanaleksic@gmail.com>
2023-12-01 17:04:38 +0100

    shell script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s nullglob globstar
+
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export SCRIPT_DIR
+
+$SCRIPT_DIR/zig-out/bin/adventofcode2023 1 1 data/input-1-1.txt
+$SCRIPT_DIR/zig-out/bin/adventofcode2023 1 2 data/input-1-1.txt
--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,12 @@ const util = @import("util.zig");
 const day1 = @import("day1.zig");
 
 pub fn main() !void {
-    std.debug.print("Advent of Code 2023 by milan@aleksic.dev\n", .{});
     var args = process.args();
     _ = args.skip();
     const day: i32 = try std.fmt.parseInt(i32, args.next() orelse "0", 10);
     const part: i32 = try std.fmt.parseInt(i32, args.next() orelse "0", 10);
     const inputFile: []const u8 = args.next() orelse "undefined";
-    std.debug.print("Running day {d}, part {d}, input file={s}\n", .{ day, part, inputFile });
+    std.debug.print("Advent of Code 2023 by milan@aleksic.dev: running day {d}, part {d}, input file={s}\n", .{ day, part, inputFile });
 
     var list = std.ArrayList([]const u8).init(allocator);
     defer list.deinit();
@@ -20,17 +19,11 @@ pub fn main() !void {
     switch (day) {
         1 => {
             switch (part) {
-                1 => {
-                    const answer = try day1.firstDay1(list);
-                    std.debug.print("Answer is {d}\n", .{answer});
-                },
-                2 => {
-                    const answer = try day1.firstDay2(list);
-                    std.debug.print("Answer is {d}\n", .{answer});
-                },
-                else => unreachable,
+                1 => std.debug.print("Answer is {d}\n", .{try day1.firstDay1(list)}),
+                2 => std.debug.print("Answer is {d}\n", .{try day1.firstDay2(list)}),
+                else => std.debug.print("Unknown / not ready implementation for part {d}\n", .{part}),
             }
         },
-        else => unreachable,
+        else => std.debug.print("Unknown / not ready implementation for day {d}\n", .{part}),
     }
 }
