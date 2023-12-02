f0417d3ab318e5d3fa7d897afc03bea8b640ee9f
Milan Aleksić <milanaleksic@gmail.com>
2023-12-02 10:35:34 +0100

    make run.sh self-maintainable

--- run.sh
+++ run.sh
@@ -8,7 +8,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export SCRIPT_DIR
 
 zig build
-$SCRIPT_DIR/zig-out/bin/adventofcode2023 1 1 data/input-1-1.txt
-$SCRIPT_DIR/zig-out/bin/adventofcode2023 1 2 data/input-1-1.txt
-$SCRIPT_DIR/zig-out/bin/adventofcode2023 2 1 data/input-2-1.txt
-$SCRIPT_DIR/zig-out/bin/adventofcode2023 2 2 data/input-2-1.txt
+BINARY="$SCRIPT_DIR/zig-out/bin/adventofcode2023"
+for i in $(seq 1 25);
+do
+    if [ -e "data/input-${i}-1.txt" ]
+    then
+        $BINARY $i 1 data/input-${i}-1.txt
+        if [ -e "data/input-${i}-2.txt" ]
+        then
+            $BINARY $i 2 data/input-${i}-2.txt
+        else
+            $BINARY $i 2 data/input-${i}-1.txt
+        fi
+    fi
+done
