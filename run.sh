d0905b69edc474b6601cc35b0158348041f47cb2
Milan Aleksić <milanaleksic@gmail.com>
2023-12-06 06:55:25 +0100

    add timing

--- run.sh
+++ run.sh
@@ -7,18 +7,29 @@ shopt -s nullglob globstar
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export SCRIPT_DIR
 
-zig build
-BINARY="$SCRIPT_DIR/zig-out/bin/adventofcode2023"
+echo 'building ReleaseFast...'
+zig build -Doptimize=ReleaseFast
+
+run() {
+    ts=$(date +%s%N)
+    "$SCRIPT_DIR/zig-out/bin/adventofcode2023" $1 $2 $3
+    echo "Done in $((($(date +%s%N) - $ts)/1000000))ms"
+}
+
+echo 'running...'
+tsTotal=$(date +%s%N)
 for i in $(seq 1 25);
 do
     if [ -e "data/input-${i}-1.txt" ]
     then
-        $BINARY $i 1 data/input-${i}-1.txt
+        run $i 1 data/input-${i}-1.txt
+        ts=$(date +%s%N)
         if [ -e "data/input-${i}-2.txt" ]
         then
-            $BINARY $i 2 data/input-${i}-2.txt
+            run $i 2 data/input-${i}-2.txt
         else
-            $BINARY $i 2 data/input-${i}-1.txt
+            run $i 2 data/input-${i}-1.txt
         fi
     fi
 done
+echo "Total time running: $((($(date +%s%N) - $tsTotal)/1000000))ms"
