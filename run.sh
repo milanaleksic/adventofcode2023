eab263e2a4fe0736b70abd63d66f3ca5643e4fc7
Milan Aleksić <milanaleksic@gmail.com>
2023-12-08 13:39:23 +0100

    chore/reviewOutput (#1)
    Reviewed-on: https://git.milanaleksic.net/milanaleksic/adventofcode2023/pulls/1
    Co-authored-by: Milan Aleksić <milanaleksic@gmail.com>
    Co-committed-by: Milan Aleksić <milanaleksic@gmail.com>

--- run.sh
+++ run.sh
@@ -10,26 +10,37 @@ export SCRIPT_DIR
 echo 'building ReleaseFast...'
 zig build -Doptimize=ReleaseFast
 
+set +u
+if [[ x"${GITHUB_STEP_SUMMARY}" == "x" ]]; then 
+    output=$(mktemp)
+else
+    output=$GITHUB_STEP_SUMMARY
+fi
+set -u
+
 run() {
+    echo "\`\`\`" | tee -a "$4"
     ts=$(date +%s%N)
-    "$SCRIPT_DIR/zig-out/bin/adventofcode2023" $1 $2 $3
-    echo "Done in $((($(date +%s%N) - $ts)/1000000))ms"
+    "$SCRIPT_DIR/zig-out/bin/adventofcode2023" $1 $2 $3 2>&1 | tee -a "$4"
+    echo "\`\`\`" | tee -a "$4"
+    echo "Done in *$((($(date +%s%N) - $ts)/1000000))ms*" 2>&1 | tee -a "$4"
 }
 
-echo 'running...'
+echo "**Starting to run the application in the release mode**" | tee -a "$output"
 tsTotal=$(date +%s%N)
 for i in $(seq 1 25);
 do
     if [ -e "data/input-${i}-1.txt" ]
     then
-        run $i 1 data/input-${i}-1.txt
+        run $i 1 data/input-${i}-1.txt "$output"
         ts=$(date +%s%N)
         if [ -e "data/input-${i}-2.txt" ]
         then
-            run $i 2 data/input-${i}-2.txt
+            run $i 2 data/input-${i}-2.txt "$output"
         else
-            run $i 2 data/input-${i}-1.txt
+            run $i 2 data/input-${i}-1.txt "$output"
         fi
     fi
 done
-echo "Total time running: $((($(date +%s%N) - $tsTotal)/1000000))ms"
+echo "Total time running: *$((($(date +%s%N) - $tsTotal)/1000000))ms*" | tee -a "$output"
+echo "Additionally, output is available in $output"
