2629fbe8f83b3440e45a2ac9a62e537c3fca5168
Milan Aleksić <milanaleksic@gmail.com>
2023-12-09 08:19:56 +0100

    move cycle detection proof into a test

--- src/day8.zig
+++ src/day8.zig
@@ -9,34 +9,48 @@ const Node = struct {
     rightId: []const u8,
 };
 
+const Data = struct {
+    const Self = @This();
+    instructions: []const u8,
+    map: std.StringHashMap(Node),
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator, list: std.ArrayList([]const u8)) !Self {
+        var instructions: ?[]const u8 = null;
+        var map: std.StringHashMap(Node) = std.StringHashMap(Node).init(allocator);
+        for (list.items) |line| {
+            // print("line={s}\n", .{line});
+            if (line.len == 0) {
+                continue;
+            }
+            if (instructions == null) {
+                instructions = try allocator.dupe(u8, line);
+            } else {
+                var splitIter = mem.split(u8, line, "=");
+                const id = mem.trim(u8, splitIter.next().?, " ");
+                const coordinates = mem.trim(u8, splitIter.next().?, " ");
+                try map.put(id, .{
+                    .leftId = coordinates[1..4],
+                    .rightId = coordinates[6..9],
+                });
+            }
+        }
+        return Self{
+            .instructions = instructions.?,
+            .map = map,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.instructions);
+        self.map.deinit();
+    }
+};
+
 pub fn part1(allocator: std.mem.Allocator, list: std.ArrayList([]const u8)) !i64 {
-    var instructions: ?[]const u8 = null;
-    defer {
-        if (instructions) |instructionsRaw| {
-            allocator.free(instructionsRaw);
-        }
-    }
-
-    var map: std.StringHashMap(Node) = std.StringHashMap(Node).init(allocator);
-    defer map.deinit();
-
-    for (list.items) |line| {
-        // print("line={s}\n", .{line});
-        if (line.len == 0) {
-            continue;
-        }
-        if (instructions == null) {
-            instructions = try allocator.dupe(u8, line);
-        } else {
-            var splitIter = mem.split(u8, line, "=");
-            const id = mem.trim(u8, splitIter.next().?, " ");
-            const coordinates = mem.trim(u8, splitIter.next().?, " ");
-            try map.put(id, .{
-                .leftId = coordinates[1..4],
-                .rightId = coordinates[6..9],
-            });
-        }
-    }
+    var data = try Data.init(allocator, list);
+    defer data.deinit();
 
     // print("instructions=[{s}]\n", .{instructions.?});
     // var mapIter = map.iterator();
@@ -50,13 +64,13 @@ pub fn part1(allocator: std.mem.Allocator, list: std.ArrayList([]const u8)) !i64
     while (stepCount < 1000000) {
         stepCount += 1;
 
-        const node = map.get(pointer).?;
-        switch (instructions.?[instructionPointer]) {
+        const node = data.map.get(pointer).?;
+        switch (data.instructions[instructionPointer]) {
             'L' => pointer = node.leftId,
             'R' => pointer = node.rightId,
             else => @panic("can not handle instruction"),
         }
-        instructionPointer = (instructionPointer + 1) % instructions.?.len;
+        instructionPointer = (instructionPointer + 1) % data.instructions.len;
         if (mem.eql(u8, pointer, "ZZZ")) {
             break;
         }
@@ -106,52 +120,23 @@ test "part 1 full" {
 }
 
 pub fn part2(allocator: std.mem.Allocator, list: std.ArrayList([]const u8)) !i64 {
-    var instructions: ?[]const u8 = null;
-    defer {
-        if (instructions) |instructionsRaw| {
-            allocator.free(instructionsRaw);
-        }
-    }
-
-    var map: std.StringHashMap(Node) = std.StringHashMap(Node).init(allocator);
-    defer map.deinit();
-
-    for (list.items) |line| {
-        // print("line={s}\n", .{line});
-        if (line.len == 0) {
-            continue;
-        }
-        if (instructions == null) {
-            instructions = try allocator.dupe(u8, line);
-        } else {
-            var splitIter = mem.split(u8, line, "=");
-            const id = mem.trim(u8, splitIter.next().?, " ");
-            const coordinates = mem.trim(u8, splitIter.next().?, " ");
-            try map.put(id, .{
-                .leftId = coordinates[1..4],
-                .rightId = coordinates[6..9],
-            });
-        }
-    }
+    var data = try Data.init(allocator, list);
+    defer data.deinit();
 
     var mapOfVisitedPoints: std.StringHashMap(i64) = std.StringHashMap(i64).init(allocator);
     defer mapOfVisitedPoints.deinit();
 
-    var iter = map.iterator();
+    var iter = data.map.iterator();
     while (iter.next()) |entry| {
         if (mem.endsWith(u8, entry.key_ptr.*, "A")) {
             const start = entry.key_ptr.*;
-
-            // uncomment to run cycle analysis on input data and proof LCM is good enough
-            // try cycleAnalysis(allocator, map, start, instructions.?);
-
             var instructionPointer: usize = 0;
             var pointer = start;
             var stepCount: i64 = 0;
             while (stepCount < 1_000_000) {
                 stepCount += 1;
-                const node = map.get(pointer).?;
-                const newPointer = switch (instructions.?[instructionPointer]) {
+                const node = data.map.get(pointer).?;
+                const newPointer = switch (data.instructions[instructionPointer]) {
                     'L' => node.leftId,
                     'R' => node.rightId,
                     else => @panic("can not handle instruction"),
@@ -161,7 +146,7 @@ pub fn part2(allocator: std.mem.Allocator, list: std.ArrayList([]const u8)) !i64
                     try mapOfVisitedPoints.put(start, stepCount);
                     break;
                 }
-                instructionPointer = (instructionPointer + 1) % instructions.?.len;
+                instructionPointer = (instructionPointer + 1) % data.instructions.len;
             }
         }
     }
@@ -189,12 +174,46 @@ fn lcm(map: std.StringHashMap(i64)) i64 {
     return @bitCast(x);
 }
 
-fn cycleAnalysis(allocator: std.mem.Allocator, map: std.StringHashMap(Node), start: []const u8, instructions: []const u8) !void {
-    var mapCycleDetection: std.StringHashMap(usize) = std.StringHashMap(usize).init(allocator);
+test "part 2 test 1" {
+    var list = try util.parseToListOfStrings([]const u8,
+        \\LR
+        \\
+        \\11A = (11B, XXX)
+        \\11B = (XXX, 11Z)
+        \\11Z = (11B, XXX)
+        \\22A = (22B, XXX)
+        \\22B = (22C, 22C)
+        \\22C = (22Z, 22Z)
+        \\22Z = (22B, 22B)
+        \\XXX = (XXX, XXX)
+    );
+    defer list.deinit();
+
+    const testValue: i64 = try part2(std.testing.allocator, list);
+    try std.testing.expectEqual(testValue, 6);
+}
+
+test "part 2 cycle analysis" {
+    var input = try util.openFile(std.testing.allocator, "data/input-8-1.txt");
+    defer input.deinit();
+
+    var data = try Data.init(std.testing.allocator, input.lines);
+    defer data.deinit();
+
+    var iter = data.map.iterator();
+    while (iter.next()) |entry| {
+        if (mem.endsWith(u8, entry.key_ptr.*, "A")) {
+            try std.testing.expect(try cycleAnalysis(data.map, entry.key_ptr.*, data.instructions));
+        }
+    }
+}
+
+fn cycleAnalysis(map: std.StringHashMap(Node), start: []const u8, instructions: []const u8) !bool {
+    var mapCycleDetection: std.StringHashMap(usize) = std.StringHashMap(usize).init(std.testing.allocator);
     defer {
         var keyIter = mapCycleDetection.keyIterator();
         while (keyIter.next()) |k| {
-            allocator.free(k.*);
+            std.testing.allocator.free(k.*);
         }
         mapCycleDetection.deinit();
     }
@@ -220,12 +239,12 @@ fn cycleAnalysis(allocator: std.mem.Allocator, map: std.StringHashMap(Node), sta
             // print("ending detected, stepOfEnding={d}\n", .{stepOfEnding});
         }
 
-        const key = try std.fmt.allocPrint(allocator, "{s}{d}", .{ pointer, instructionPointer });
+        const key = try std.fmt.allocPrint(std.testing.allocator, "{s}{d}", .{ pointer, instructionPointer });
         if (mapCycleDetection.get(key)) |existing| {
             // print("cycle detected at {d}, lengthOfCycle={d}\n", .{ stepCount, stepCount - existing });
             stepOfCycleStart = existing;
             stepOfCycleEnd = stepCount;
-            allocator.free(key);
+            std.testing.allocator.free(key);
             break;
         } else {
             try mapCycleDetection.put(key, stepCount);
@@ -233,26 +252,7 @@ fn cycleAnalysis(allocator: std.mem.Allocator, map: std.StringHashMap(Node), sta
         instructionPointer = (instructionPointer + 1) % instructions.len;
     }
     // print("stepOfEnding={d} stepOfCycleStart={d}, stepOfCycleEnd={d}\n", .{ stepOfEnding, stepOfCycleStart, stepOfCycleEnd });
-    std.debug.assert(stepOfEnding == stepOfCycleEnd - stepOfCycleStart);
-}
-
-test "part 2 test 1" {
-    var list = try util.parseToListOfStrings([]const u8,
-        \\LR
-        \\
-        \\11A = (11B, XXX)
-        \\11B = (XXX, 11Z)
-        \\11Z = (11B, XXX)
-        \\22A = (22B, XXX)
-        \\22B = (22C, 22C)
-        \\22C = (22Z, 22Z)
-        \\22Z = (22B, 22B)
-        \\XXX = (XXX, XXX)
-    );
-    defer list.deinit();
-
-    const testValue: i64 = try part2(std.testing.allocator, list);
-    try std.testing.expectEqual(testValue, 6);
+    return stepOfEnding == stepOfCycleEnd - stepOfCycleStart;
 }
 
 test "part 2 full" {
